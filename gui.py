e6a86aaa22fd34eca20f6943feeadb4ad66e994e
akshaynaik797 <akshaynaik797@gmail.com>
2020-09-18 17:38:04 +0530

    finished 1.0

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
 from tkinter import Tk, Label, Entry, StringVar, Button, ttk, messagebox, Scrollbar, BOTH, Listbox, RIGHT, END
 from backend import accept_values
+from from_delete import accept_data
 import tkinter
 
+my_date = ''
 mygui = Tk(className='Process Failed Settlements')
 mygui.geometry("900x200")
 
@@ -41,23 +43,27 @@ def send_values():
         messagebox.showerror(title="Error", message='Enter values in all fields')
 
 def check_in_db():
+    global my_date
     win = tkinter.Toplevel()
     win.wm_title("Check db")
     win.geometry("600x200")
     format_label = Label(win, text='dd/mm/yyyy hh:mm:ss').grid(row=2, column=1)
     # format_label2 = Label(win, text='dd/mm/yyyy hh:mm:ss').grid(row=2, column=4)
 
-    fromtime = StringVar()
-    fromtime_label = Label(win, text="From", width=10).grid(row=1, column=0)
-    fromtime_entry = Entry(win, textvariable=fromtime).grid(row=1, column=1, pady=15)
-    fromtime.set("01/08/2020 00:00:01")
+    fromtime1 = StringVar()
+    fromtime_label1 = Label(win, text="From", width=10).grid(row=1, column=0)
+    fromtime_entry1 = Entry(win, textvariable=fromtime).grid(row=1, column=1, pady=15)
+    fromtime1.set("01/08/2020 00:00:01")
+    my_date = fromtime1.get()
+
+
 
     # totime = StringVar()
     # totime_label = Label(win, text="To", width=10).grid(row=1, column=3)
     # totime_entry = Entry(win, textvariable=totime).grid(row=1, column=4)
     # totime.set("01/09/2020 00:00:01")
-    checkButton = Button(win, text="Process", command=win.destroy).grid(row=5, column=1)
-    closeButton = Button(win, text="Close", command=win.destroy).grid(row=5, column=2)
+    checkButton1 = Button(win, text="Check Deleted", command=check_deleted).grid(row=5, column=1)
+    closeButton1 = Button(win, text="Close", command=win.destroy).grid(row=5, column=2)
 
 def check_deleted():
     root = tkinter.Toplevel()
@@ -68,19 +74,18 @@ def check_deleted():
     scrollbar.pack(side=RIGHT, fill=BOTH)
     listbox = Listbox(root)
     listbox.pack()
-
-    for i in range(100):
-        j = "aks, ads,aks, ads,aks, ads,aks, ads, ads " + str(i)
+    temp = accept_data(my_date)
+    for j in temp:
         listbox.insert(END, j)
 
     # bind listbox to scrollbar
     listbox.config(yscrollcommand=scrollbar.set, width=100)
     scrollbar.config(command=listbox.yview)
-    checkButton1 = Button(root, text="Process pdfs", command=check_in_db).pack()
+    # checkButton1 = Button(root, text="Process pdfs", command=check_in_db).pack()
     closeButton1 = Button(root, text="Close", command=root.destroy).pack()
 
 
 loginButton = Button(mygui, text="Submit", command=send_values).grid(row=5, column=0)
-checkButton = Button(mygui, text="Check Deleted", command=check_deleted).grid(row=5, column=1)
+checkButton = Button(mygui, text="Check Deleted", command=check_in_db).grid(row=5, column=1)
 closeButton = Button(mygui, text="Close", command=mygui.destroy).grid(row=5, column=2)
 mygui.mainloop()
