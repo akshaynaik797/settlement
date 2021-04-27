31e473bdb2e8cb0dbf21f3e5a57e9bfb12a9a4c4
akshay naik <akshaynaik797@gmail.com>
2021-04-27 11:22:52 +0530

    0.0.39 done genins and health heritage

--- 1.py
+++ 1.py
@@ -3,6 +3,6 @@ import os
 
 ins = 'temp'
 mid = '177fbf009add6841'
-filepath = '/home/akshay/temp/6650_Claim_Payment_Hospital_NEFT_3812099.pdf'
+filepath = '/home/akshay/temp/98455637_.pdf'
 
 subprocess.run(["python", 'pdf_' + ins + ".py", filepath, mid])
--- pdf_genins.py
+++ pdf_genins.py
@@ -15,26 +15,26 @@ try:
     #     "CorporateName", "MemberID", "Diagnosis", "Discount", "Copay")
 
     regex_dict = {
-        'ClaimNo': [[r"(?<=CCN).*(?=\))"], [':'], r"^\S+$"],
-        'PatientName': [[r"(?<=Patient Name).*(?=Age)"], [':'], r"^\S+(?: \S+)*$"],
-        'POLICYNO': [[r"(?<=Policy No).*"], [':', '.'], r"^\S+$"],
-        'DateofAdmission': [[r"(?<=DOA).*(?=DOD)"], [':'], r"^\S+(?: \S+)*$"],
-        'DateofDischarge': [[r"(?<=DOD).*"], [':'], r"^\S+(?: \S+)*$"],
+        'ClaimNo': [[r"(?<=Claim Control Number).*"], [':'], r"^\S+$"],
+        'PatientName': [[r"(?<=Claim in respect of).*"], [':'], r"^\S+(?: \S+)*$"],
+        'POLICYNO': [[r"(?<=Policy Number).*(?=Reference)"], [':', '.'], r"^\S+$"],
+        'DateofAdmission': [[r"(?<=Date of Admission).*(?=Date)"], [':'], r"^\S+(?: \S+)*$"],
+        'DateofDischarge': [[r"(?<=Date of Discharge).*"], [':'], r"^\S+(?: \S+)*$"],
         'InsurerID': [[r"(?<=Insurer Co.).*(?=Policy No)"], [':', '.'], r"^.*$"],
         'CorporateName': [[r"(?<=Group Name).*(?=Date)"], [':'], r"^.*$"],
-        'MemberID': [[r"(?<=I-Card No).*(?=Relation)"], ['.', ':'], r"^.*$"],
+        'MemberID': [[r"(?<=ID Card Number).*"], ['.', ':'], r"^.*$"],
         'Diagnosis': [[r"(?<=Primary Diagnosis).*(?=ICD Code)"], [':'], r"^.*$"],
 
-        'UTRNo': [[r"(?<=NEFT No).*(?=Date)"], [':', '.'], r"^\S+$"],
-        'Transactiondate': [[r"(?<=Date of Transfer).*(?=Settled)"], [':'], ""],
+        'UTRNo': [[r"(?<=Reference/UTR No).*"], [':', '.'], r"^\S+$"],
+        'Transactiondate': [[], [':'], ""],
         'AccountNo': [[], [':'], r"^\S+(?: \S+)*$"],
         'BeneficiaryBank_Name': [[], [':'], r"^\S+(?: \S+)*$"],
 
-        'BilledAmount': [[r"(?<=Amount Claimed).*"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
-        'SettledAmount': [[r"(?<=Settled Amount).*"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
-        'NetPayable': [[r"(?<=Net Amount).*"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
+        'BilledAmount': [[r"(?<=Amount claimed).*(?=ed)"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
+        'SettledAmount': [[r"(?<=Gross amount settled).*"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
+        'NetPayable': [[r"(?<=Net amount paid).*"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
         'Copay': [[], [':'], r"^\S+(?: \S+)*$"],
-        'TDS': [[r"(?<=TDS Deducted).*(?=Deducted)"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
+        'TDS': [[r"(?<=TDS deducted).*"], [':', 'Rs.', 'INR', '/-'], r"^\d+(?:\.\d+)*$"],
         'Discount': [[], [], r"^.*$"]
     }
     datadict = get_data_dict(regex_dict, f)
@@ -46,18 +46,17 @@ try:
     #     "Discount", "DeductionCategory", "MailID", "HospitalID", "stgsettlement_sno")
 
     x1 = ""
-    regex = r"(?<=Details of deductions:\n)[\s\S]*(?=\n.*MOU Discount)"
-    if data := re.search(regex, f):
-        data = data.group().strip().split('\n')
-
+    regex = r"(?<=DEDUCTION ::).*"
+    data = re.findall(regex, f)
+    data = [i.strip() for i in data]
 
     for j, i in enumerate(data):
-        if tmp := re.search(r"((?<=Rs.) *\d+(?:.?\d+) )(.*)", i):
+        if tmp := re.search(r"(?P<deduction>\d+)(/-)(?P<reason>.*)", i):
             data[j] = tmp.groups()
     deductions = []
     for i in data:
         tmp = {}
-        tmp["DeductedAmt"], tmp["DeductionReason"] = i
+        tmp["PayableAmount"], _, tmp["DeductionReason"] = i
         tmp["MailID"], tmp["HospitalID"] = mail_id, hospital
         tmp["TPAID"], tmp["ClaimID"] = datadict["TPAID"], datadict["ClaimNo"]
         deductions.append(tmp)
